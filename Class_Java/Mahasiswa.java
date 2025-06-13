cc0736dfa45f46ac06831f9bc3af7a106b7ba8f2
BunchFlyder7 <yazidbaidha07@gmail.com>
2025-06-13 16:42:38 +0700

    Update Mahasiswa.java

--- Class_Java/Mahasiswa.java
+++ Class_Java/Mahasiswa.java
@@ -1,18 +1,17 @@
 package Class_Java;
-import java.util.ArrayList;
-import java.util.List;
+
 public class Mahasiswa {
     //Atribut Mahasiswa
     private String Nama;
     private String NIM;
     private double Denda;
-    private List <Buku> Daftar_Buku_Dipinjam;
+    
     //Constructor
     public Mahasiswa (String Nama, String NIM){
         this.Nama  = Nama;
         this.NIM   = NIM;
         this.Denda = 0.0;
-        this.Daftar_Buku_Dipinjam = new ArrayList<>();
+        
     }
     public Mahasiswa (){
     }
@@ -26,9 +25,6 @@ public class Mahasiswa {
     public void setDenda(double Denda){
         this.Denda = Denda;
     }
-    public void add_Buku_Dipinjam(Buku buku){
-        this.Daftar_Buku_Dipinjam.add(buku);
-    }
     public String getNama(){
         return Nama;
     }
@@ -37,14 +33,5 @@ public class Mahasiswa {
     }
     public double getDenda(){
         return Denda;
-    }
-    public void tampilkanDaftar_Buku_Dipinjam(){
-        System.out.println("Daftar Buku Dipinjam: ");
-        for(Buku b : Daftar_Buku_Dipinjam){
-            System.out.println("- " + b.getJudul());
-        }
-    }
-    public void ubahDenda(double Denda){
-        setDenda(getDenda() + Denda);
     }
 }
